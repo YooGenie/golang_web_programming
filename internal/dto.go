284e603f6ba473c8498662b55e086002f1861bd8
YooGenie <92665524+YooGenie@users.noreply.github.com>
2022-08-28 22:05:44 +0900

    [WEEK 1] 유선희 과제 제출 (#39)
    * feat: 샘플 테스트 코드 추가
    
    * test: 1주차 과제1
    
    * test: 1주차 과제3
    
    Co-authored-by: moonyoung <moonyoung.dev@kakao.com>

---
+++ internal/dto.go
@@ -0,0 +1,29 @@
+package internal
+
+type CreateRequest struct {
+	UserName       string
+	MembershipType string
+}
+
+type CreateResponse struct {
+	ID             string
+	MembershipType string
+}
+
+type UpdateRequest struct {
+	ID             string
+	UserName       string
+	MembershipType string
+}
+
+type UpdateResponse struct {
+	ID             string
+	UserName       string
+	MembershipType string
+}
+
+type GetResponse struct {
+	ID             string
+	UserName       string
+	MembershipType string
+}
