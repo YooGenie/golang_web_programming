fa387ec8e96ff941c9a1c734fbf4f274525df9c1
ysun7 <ysun7@beautifulstore.org>
2022-09-11 14:09:52 +0900

    feat: 220828 실습

--- cmd/main.go
+++ cmd/main.go
@@ -5,4 +5,8 @@ import "golang_web_programming/internal"
 func main() {
 	server := internal.NewDefaultServer()
 	server.Run()
+
+	//e:= echo.New()
+	//e.GET("/", func(c echo.Context) error { return c.String(c.NoContent(http.StatusOK), "hello world") })
+	//e.Logger.Fatal(e.Start(":8080"))
 }
