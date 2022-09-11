b418d5310d94136aaf3d55e0f198271bfa8b53d8
ysun7 <ysun7@beautifulstore.org>
2022-09-11 15:34:23 +0900

    feat: 2주차 과제1

--- internal/dto.go
+++ internal/dto.go
@@ -1,18 +1,13 @@
 package internal
 
-type CreateRequest struct {
-	UserName       string `json:"user_name"`
-	MembershipType string `json:"membership_type"`
+type Request struct {
+	ID             string `json:"ID" validate:"omitempty"`
+	UserName       string `json:"userName" validate:"required"`
+	MembershipType string `json:"membershipType" validate:"required"`
 }
 
 type CreateResponse struct {
 	ID             string `json:"id"`
-	MembershipType string `json:"membership_type"`
-}
-
-type UpdateRequest struct {
-	ID             string `json:"id"`
-	UserName       string `json:"user_name"`
 	MembershipType string `json:"membership_type"`
 }
 
